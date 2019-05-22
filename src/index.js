68116ff4361cecca6a28be8cca54b17b38aa462b
Bastijn Vissers <bastijn.vissers@philips.com>
2019-05-22 12:56:11 +0200

    Fix parsing issue for json files with different encodings (windows issues)

--- src/index.js
+++ src/index.js
@@ -36,6 +36,42 @@ const errorMessage = (message, verboseMessage) => {
   }
 };
 
+const GetFileEncodingHeader = (filePath) => {
+  const readStream = fs.openSync(filePath, 'r');
+  const bufferSize = 2;
+  const buffer = new Buffer(bufferSize);
+  let readBytes = 0;
+
+  if (readBytes = fs.readSync(readStream, buffer, 0, bufferSize, 0)) {
+      return buffer.slice(0, readBytes).toString("hex");
+  }   
+
+  return "";
+}
+
+const ReadFileSyncUtf8 = (filePath) => {
+  const fileEncoding = GetFileEncodingHeader(filePath);
+  let content = null;
+
+  if (fileEncoding === "fffe" || fileEncoding === "utf16le") {
+      content = fs.readFileSync(filePath, "ucs2"); // utf-16 Little Endian
+  } else if (fileEncoding === "feff" || fileEncoding === "utf16be") {
+      content = fs.readFileSync(filePath, "uts2").swap16(); // utf-16 Big Endian
+  } else {
+      content = fs.readFileSync(filePath, "utf8");
+  }
+
+  // trim removes the header...but there may be a better way!
+  return content.toString("utf8").trimStart();
+}
+
+const GetJson = (filePath) => {
+  const jsonContents = ReadFileSyncUtf8(filePath);
+  infoMessage(`Found file encoding header: ` + GetFileEncodingHeader(filePath));
+
+  return JSON.parse(jsonContents);
+}
+
 const processFiles = async () => {
   let dependencies;
   infoMessage(
@@ -44,7 +80,7 @@ const processFiles = async () => {
   );
 
   try {
-    dependencies = await fs.readJSON(input);
+    dependencies = GetJson(input);
   } catch (e) {
     errorMessage(chalk`Could not open {blue ${input}} for reading...`, e);
     return;
